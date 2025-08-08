b74a3418e51527c0a6c41bf2ff3006602b47124b
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-08-08 15:42:22 +0300

    transfer template and add first components

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', \App\Livewire\HomeComponent::class)->name('home');
+Route::get('/category', \App\Livewire\Product\CategoryComponent::class)->name('category');
+Route::get('/product', \App\Livewire\Product\ProductComponent::class)->name('product');
+
