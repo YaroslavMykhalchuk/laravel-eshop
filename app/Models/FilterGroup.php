828a4bc48c0d9b73e905c2f6a90004036cea7649
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-08-12 18:21:52 +0300

    fill model for filter

--- app/Models/FilterGroup.php
+++ app/Models/FilterGroup.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class FilterGroup extends Model
 {
-    //
+    protected $table = 'filter_groups';
+
+    protected $fillable = [
+        'title',
+    ];
+
+    public $timestamps = false;
 }
