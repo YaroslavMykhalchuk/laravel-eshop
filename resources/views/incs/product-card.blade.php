2ed95c8dc169f223a24ead05027140b987570312
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-08-20 01:06:41 +0300

    fix product-card.blade.php

--- resources/views/incs/product-card.blade.php
+++ resources/views/incs/product-card.blade.php
@@ -8,13 +8,13 @@
         @endif
     </div>
     <div class="product-thumb">
-        <a href="{{ route('product', $product->slug) }}">
+        <a href="{{ route('product', $product->slug) }}" wire:navigate>
             <img src="{{ asset($product->getImage()) }}" alt="">
         </a>
     </div>
     <div class="product-details">
         <h4>
-            <a href="{{ route('product', $product->slug) }}">{{ $product->title }}</a>
+            <a href="{{ route('product', $product->slug) }}" wire:navigate>{{ $product->title }}</a>
         </h4>
         <div class="product-bottom-details d-flex justify-content-between">
             <div class="product-price">
