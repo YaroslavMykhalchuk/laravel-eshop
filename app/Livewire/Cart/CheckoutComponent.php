3d9d43e8d84fb0d6ba211a5aff34b55e6996b77c
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-08-20 23:46:29 +0300

    fix checkout errur

--- app/Livewire/Cart/CheckoutComponent.php
+++ app/Livewire/Cart/CheckoutComponent.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
+use mysql_xdevapi\Exception;
 
 class CheckoutComponent extends Component
 {
@@ -54,14 +55,24 @@ class CheckoutComponent extends Component
             }
             $order->orderProducts()->createMany($order_products);
 
-            Mail::to($validated['email'])->send(new OrderClient($order_products, \App\Helpers\Cart\Cart::getCartTotalSum(), $order->id, $validated['note'] ?? ''));
-            Mail::to('manager@laravel-eshop.loc')->send(new OrderManager($order->id));
+            DB::commit();
+
+            try {
+                Mail::to($validated['email'])
+                    ->send(new OrderClient(
+                        $order_products,
+                        \App\Helpers\Cart\Cart::getCartTotalSum(),
+                        $order->id, $validated['note'] ?? ''));
+                Mail::to('manager@laravel-eshop.loc')
+                    ->send(new OrderManager($order->id));
+            } catch (\Exception $e ) {
+                Log::error($e->getMessage());
+            }
 
             \App\Helpers\Cart\Cart::clearCart();
             $this->dispatch('cart-updated');
-
             $this->js("toastr.success('Order created successfully!')");
-            DB::commit();
+
         } catch ( \Exception $e ) {
             DB::rollBack();
             Log::error($e->getMessage());
