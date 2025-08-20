79fe897d07ed43f7a43b32569fa4af9a98a15fc1
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-08-20 17:26:38 +0300

    add user account

---
+++ app/Livewire/User/ChangeAccountComponent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Livewire\User;
+
+use App\Models\User;
+use Livewire\Component;
+
+class ChangeAccountComponent extends Component
+{
+    public string $name;
+    public string $email;
+    public string $password = '';
+
+    public function mount()
+    {
+        $this->name = auth()->user()->name;
+        $this->email = auth()->user()->email;
+    }
+
+    public function save()
+    {
+        $user = User::query()->findOrFail(auth()->id());
+        $validated = $this->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . auth()->id(),
+            'password' => 'nullable|min:6|max:255',
+        ]);
+        if(!$validated['password']) {
+            unset($validated['password']);
+        }
+        $user->update($validated);
+
+        session()->flash('success', 'Account details updated successfully!');
+        $this->redirectRoute('login', navigate: true);
+    }
+
+    public function render()
+    {
+        return view('livewire.user.change-account-component');
+    }
+}
