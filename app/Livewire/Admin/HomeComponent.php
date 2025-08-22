f5711bb0909382dc130d8a93d5f54016b9e516c1
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-08-22 18:56:54 +0300

    add dashboard page

--- app/Livewire/Admin/HomeComponent.php
+++ app/Livewire/Admin/HomeComponent.php
@@ -2,14 +2,24 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Layout('components.layouts.admin')]
+#[Title('Dashboard')]
 class HomeComponent extends Component
 {
-    #[Layout('components.layouts.admin')]
     public function render()
     {
-        return view('livewire.admin.home-component');
+        $products_cnt = Product::query()->count();
+        $users_cnt = User::query()->count();
+        $orders_cnt = Order::query()->count();
+        $orders_total = Order::query()->sum('total');
+
+        return view('livewire.admin.home-component', compact('products_cnt', 'users_cnt', 'orders_cnt', 'orders_total'));
     }
 }
--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -48,19 +48,13 @@
         <li class="nav-item active">
             <a class="nav-link" href="{{ route('dashboard') }}" wire:navigate>
                 <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></a>
+                <span>Dashboard</span>
+            </a>
         </li>
 
         <!-- Sidebar Toggler (Sidebar) -->
         <div class="text-center d-none d-md-inline">
             <button class="rounded-circle border-0" id="sidebarToggle"></button>
-        </div>
-
-        <!-- Sidebar Message -->
-        <div class="sidebar-card d-none d-lg-flex">
-            <img class="sidebar-card-illustration mb-2" src="{{ asset('assets/admin/img/undraw_rocket.svg') }}" alt="...">
-            <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-            <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
         </div>
 
     </ul>
@@ -82,32 +76,6 @@
 
                 <!-- Topbar Navbar -->
                 <ul class="navbar-nav ml-auto">
-
-                    <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                    <li class="nav-item dropdown no-arrow d-sm-none">
-                        <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-search fa-fw"></i>
-                        </a>
-                        <!-- Dropdown - Messages -->
-                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                             aria-labelledby="searchDropdown">
-                            <form class="form-inline mr-auto w-100 navbar-search">
-                                <div class="input-group">
-                                    <input type="text" class="form-control bg-light border-0 small"
-                                           placeholder="Search for..." aria-label="Search"
-                                           aria-describedby="basic-addon2">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fas fa-search fa-sm"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </li>
-
-
                     <!-- Nav Item - User Information -->
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
@@ -138,7 +106,7 @@
             <!-- Begin Page Content -->
             <div class="container-fluid">
 
-                <h1 class="h3 mb-0 text-gray-800">{{ $title ?? 'Admin page' }}</h1>
+                <h1 class="h3 mb-3 text-gray-800">{{ $title ?? 'Admin page' }}</h1>
 
                 {{ $slot }}
 
