8453055cc5506b5b592971b99e666fc4f5d80f9e
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-08-24 19:24:33 +0300

    add admin list categories

---
+++ resources/views/livewire/admin/category/category-index-component.blade.php
@@ -0,0 +1,25 @@
+<div class="row">
+    <div class="col-12 mb-4">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <a href="#" class="btn btn-primary">Add Category</a>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="col-1">ID</th>
+                                <th class="col-9">Title</th>
+                                <th class="col-2">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {!! \App\Helpers\Category\Category::getMenu('incs.menu-table-tpl') !!}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
