6c398e136d549bf702cafcff3598ba286ca6dd99
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-08-28 13:57:30 +0300

    add admin crud for products (deleting)

--- app/Livewire/Admin/Product/ProductIndexComponent.php
+++ app/Livewire/Admin/Product/ProductIndexComponent.php
@@ -3,6 +3,9 @@
 namespace App\Livewire\Admin\Product;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -13,6 +16,35 @@ use Livewire\WithPagination;
 class ProductIndexComponent extends Component
 {
     use WithPagination;
+
+    public function deleteProducts(Product $product)
+    {
+        try {
+            DB::beginTransaction();
+
+            DB::table('filter_products')
+                ->where('product_id', $product->id)
+                ->delete();
+            if($product->image){
+                Storage::disk('public_uploads_delete')->delete($product->image);
+            }
+            if($gallery = $product->gallery){
+                Storage::disk('public_uploads_delete')->delete($gallery);
+            }
+            $product->delete();
+
+            DB::commit();
+
+            session()->flash('success', 'Product has been deleted.');
+            return;
+        } catch (\Exception $e)
+        {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            $this->js("toastr.error('Error deleting product')");
+
+        }
+    }
 
     public function render()
     {
